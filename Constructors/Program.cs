59c64357772585b00cc33153ff417c2d0467aa23
Sami Williams <samiw0412@gmail.com>
2022-01-14 13:18:05 +0000

    Constructors exercise

---
+++ Constructors/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.Name = "John";
+
+            var order = new Order();
+            customer.Orders.Add(order);
+
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+        }
+    }
+}
