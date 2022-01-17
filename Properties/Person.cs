d15987c9aeabf6128925cca66ee2bcfeca613e48
Sami Williams <samiw0412@gmail.com>
2022-01-17 14:58:28 +0000

    Properties exercise

---
+++ Properties/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Properties
+{
+    public class Person
+    {
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+        public DateTime Birthdate { get; private set; }
+
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - Birthdate;
+                var years = timeSpan.Days / 365;
+                return years;
+            }
+        }
+    }
+}
---
+++ Properties/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person(new DateTime(1982, 1, 1));
+            Console.WriteLine(person.Age);
+        }
+    }
+}
