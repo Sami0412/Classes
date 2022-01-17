2cf6f5da692e7a515cde0548d49497adafa136cf
Sami Williams <samiw0412@gmail.com>
2022-01-17 17:12:07 +0000

    Stopwatch exercise initial commit - able to 'start' stopwatch

---
+++ stopwatch/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Dynamic;
+
+namespace stopwatch
+{
+    public class Stopwatch
+    {
+        public static DateTime StartTime { get; private set; }
+        public DateTime EndTime { get; private set; }
+
+        public static DateTime Start()
+        {
+            StartTime = DateTime.Now;
+            return StartTime;
+        }
+
+        public DateTime End()
+        {
+            EndTime = DateTime.Now;
+            return EndTime;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 'start' to start the stopwatch: ");
+            if (Console.ReadLine() == "start")
+            {
+                Stopwatch.Start();
+            }
+        }
+    }
+}
