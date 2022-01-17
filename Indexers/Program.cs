e39fb28262f02316011d3acee1e2983887142ecf
Sami Williams <samiw0412@gmail.com>
2022-01-17 16:40:25 +0000

    Indexers exercise

---
+++ Indexers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Indexers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cookie = new HttpCookie();
+            cookie["name"] = "Sami";
+            Console.WriteLine(cookie["name"]);
+        }
+    }
+}
